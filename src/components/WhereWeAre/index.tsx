aa58257fc0137cbd5bf14a6cef87f1b587ea5ad6
Emmasaku <omosakuemmanuel22@gmail.com>
2023-09-10 15:54:08 +0100

    Configured Buttons

--- src/components/WhereWeAre/index.tsx
+++ src/components/WhereWeAre/index.tsx
@@ -5,6 +5,9 @@ import DormantAlert from "@/assets/images/DormantAlert.svg";
 import DormantHand from "@/assets/images/DormantHand.svg";
 import NigerianChildMobile from "@/assets/images/NigerianChildMobile.svg";
 import NigerianChldDesktop from "@/assets/images/NigeriaChildDesktop.svg";
+import DesktopBloodAlert from "@/assets/images/Desktop_Blood_Alert.svg"
+import DesktopAlarmPicture from "@/assets/images/Desktop_Alarm_Picture.svg"
+import DesktopHelpPicture from "@/assets/images/DesktopHelpPicture.svg"
 
 const WhereWeAre = () => {
   return (
@@ -12,21 +15,22 @@ const WhereWeAre = () => {
       {/* Where we are */}
       <div className="gap-y-12 grid mt-20 mx-auto md:mt-0 md:mx-0 md:flex md:justify-between md:gap-y-0 md:grid-cols-2">
         {/* Left Section */}
-        <div className="grid grid-cols-4 gap-y-6 md:py-16 md:pl-[6.25rem]">
+        <div className="grid  grid-cols-4 gap-y-6 md:py-16 md:pl-[6.25rem] ">
           {/* Where we are button */}
-          <div className="col-start-2 col-end-4 mx-auto md:col-span-4">
+          <div className="col-start-2 col-end-4 mx-auto md:col-span-4 md:mx-0 md:px-8 md:mt-[6rem]">
             <button className="text-xs font-bold px-[1.12rem] rounded-full py-2 bg-[#FCD1CD] text-[#1A1A1A] font-lato md:text-lg md:py-[0.62rem] md:px-6">
               WHERE WE ARE
             </button>
           </div>
           {/* Section icons and buttons */}
           <div className="px-8 col-start-1 col-end-5">
-            <div className="container  grid grid-cols-4 grid-flow-row gap-x-2 gap-y-4 md:gap-y-16">
+            <div className="container grid grid-cols-4 grid-flow-row gap-x-2 gap-y-4 md:gap-y-16">
               {/* Mobile */}
               {/* Blood Shortage */}
               <div className="col-start-1 col-end-3 md:col-span-5">
                 <div className="flex space-x-1 items-center">
-                  <Image src={ActiveBlood} alt="Active Blood" />
+                  <Image src={ActiveBlood} alt="Active Blood" className="md:hidden"/>
+                  <Image src={DesktopBloodAlert} alt="Blood Alert" className="hidden md:block"/>
                   <h1 className="text-xl font-black font-lato text-[#1A1A1A] md:text-5xl">
                     Blood Shortage
                   </h1>
@@ -36,7 +40,8 @@ const WhereWeAre = () => {
               {/* Critical Impact */}
               <div className="col-start-3 col-end-5 md:col-span-5">
                 <div className="flex space-x-1 items-center">
-                  <Image src={DormantAlert} alt="Dormant Alert" />
+                  <Image src={DormantAlert} alt="Dormant Alert" className="md:hidden" />
+                  <Image src={DesktopAlarmPicture} alt="Desktop Alarm Dormant Picture" className="hidden md:block"/>
                   <h1 className="text-xl font-black font-lato text-[#D1D1D1] md:text-5xl">
                     Critical Impact
                   </h1>
@@ -46,7 +51,8 @@ const WhereWeAre = () => {
               {/* Help Available */}
               <div className="col-start-2 col-end-4 md:col-span-5">
                 <div className="flex space-x-1 items-center">
-                  <Image src={DormantHand} alt="Dormant Hand" />
+                  <Image src={DormantHand} alt="Dormant Hand" className="md:hidden" />
+                  <Image src={DesktopHelpPicture} alt="Dormant Desktop Help Picture" className="hidden md:block"/>
                   <h1 className="text-xl font-black font-lato text-[#D1D1D1] md:text-5xl">
                     Help Available
                   </h1>
