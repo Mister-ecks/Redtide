af0292d0e0cf4489479a7881d97fde250ab311bc
Emmasaku <omosakuemmanuel22@gmail.com>
2023-09-25 19:32:30 +0100

    Optimized Desktop Gallery

--- src/components/GalleryShow/index.tsx
+++ src/components/GalleryShow/index.tsx
@@ -1,37 +1,48 @@
-import React from 'react'
+import React from "react";
 import Image from "next/image";
 import ImageWithHover from "../RedTideGallery";
 import GirlPlayground from "@/assets/images/GirlPlayground.svg";
 import WomanChild from "@/assets/images/WomanChild.svg";
 import OldManDefault from "@/assets/images/OldManDefault.svg";
 
-const GalleryShowCase
- = () => {
+const GalleryShowCase = () => {
   return (
     <div>
-        <div className="mt-[2.62rem]">
+      <div className="block h-auto md:flex md:justify-between md:items-center mt-[2.62rem]">
+        <div>
           <ImageWithHover
             imageUrl={GirlPlayground}
             text="Heartbeat of Gratitude"
             additionalText="A Journey of Hope and Healing through Blood Donation"
-            marginTop={"5rem"}
-          />
-          <ImageWithHover
-            imageUrl={WomanChild}
-            text="For Love's Sake"
-            additionalText="Embracing Life's Precious Moments - The Gift of Blood Donation"
-            marginTop="5rem"
-          />
-          <ImageWithHover
-            imageUrl={OldManDefault}
-            text="The Gift of Time"
-            additionalText="A Tale of Life, Love, and Second Chances with Blood Donors"
-            marginTop="5rem"
+            marginTop={"20.06rem"}
+            marginTopDesktop={"31.12rem"}
           />
         </div>
 
-    </div>
-  )
-}
+        <div className="block md:flex md:flex-col md:space-y-[1.25rem]">
+          <div>
+            <ImageWithHover
+              imageUrl={WomanChild}
+              text="For Love's Sake"
+              additionalText="Embracing Life's Precious Moments - The Gift of Blood Donation"
+              marginTop="5rem"
+              marginTopDesktop={"11.25rem"}
+            />
+          </div>
 
-export default GalleryShowCase
+          <div>
+            <ImageWithHover
+              imageUrl={OldManDefault}
+              text="The Gift of Time"
+              additionalText="A Tale of Life, Love, and Second Chances with Blood Donors"
+              marginTop="5rem"
+              marginTopDesktop={"11.25rem"}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GalleryShowCase;
--- src/components/RedTideGallery/index.tsx
+++ src/components/RedTideGallery/index.tsx
@@ -5,17 +5,17 @@ interface ImageWithHoverProps {
     imageUrl: string;
     text: string;
     additionalText: string;
-    marginTop: string
-
+    marginTop: string;
+    marginTopDesktop: string;
   }
 
-const ImageWithHover: React.FC<ImageWithHoverProps> = ({imageUrl, text, additionalText, marginTop}) => {
+const ImageWithHover: React.FC<ImageWithHoverProps> = ({imageUrl, text, additionalText, marginTop, marginTopDesktop}) => {
   return (
     <div>
         <div className="relative">
-          <Image src={imageUrl} alt="Image" className="max-full h-auto w-auto" />
-          <div className={`absolute inset-0 bg-black opacity-0 hover:opacity-70  h-auto  transition-opacity duration-500 ease-in-out`}>
-            <div className={`font-lato flex flex-col mt-[${marginTop}] ml-[1.5rem]`}>
+          <Image src={imageUrl} alt="Image" className="max-w-full h-auto w-auto" />
+          <div className={`absolute inset-0 flex justify-start items-center bg-black opacity-0 hover:opacity-70  h-auto  transition-opacity duration-500 ease-in-out`}>
+            <div className={`font-lato flex flex-col ml-[1.5rem] mt-[${marginTop}] md:mt-[${marginTopDesktop}]`}>
               <p className="text-white text-[1.12rem]">{text}</p>
               <p className="text-white text-[0.75rem]">{additionalText}</p>
             </div>
