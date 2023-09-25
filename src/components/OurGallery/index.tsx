147a97bbca88af230bf6c350ce8ccda4fcdc575d
Emmasaku <omosakuemmanuel22@gmail.com>
2023-09-25 16:50:54 +0100

    OurGallery Section Hover Added

--- src/components/OurGallery/index.tsx
+++ src/components/OurGallery/index.tsx
@@ -1,27 +1,61 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import ImageWithHover from "../RedTideGallery";
+import GirlPlayground from "@/assets/images/GirlPlayground.svg";
+import WomanChild from "@/assets/images/WomanChild.svg";
+import OldManDefault from "@/assets/images/OldManDefault.svg";
 
 const OurGallery = () => {
   return (
     <div>
-        {/* Section Header*/}
-        {/* OurGalleryText */}
-        <section className="flex flex-col container mx-auto mt-20 px-[1.5rem] md:mt-[8.13rem] md:px-0 md:mx-0 md:ml-[6.25rem]">
-            {/* Top bottom */}
-            <div className='mb-[1.12rem]'>
-            <button className='bg-[#FCD1CD] text-[#1A1A1A] py-[0.5rem] font-lato text-[0.75rem] font-bold px-[1.12rem] inline rounded-full'>
-                OUR GALLERY
-            </button>
-            </div>
-            
-            {/* Text */}
-            <div>
-                <h1 className='text-[1.5rem] font-black text-[#1A1A1A] font-lato max-w-[17.4375rem] mb-[0.75rem] md:max-w-[67.5rem] md:text-[3rem]'>Different Shades of <span className='text-[#B80F00]'>RedTide</span> Heroes</h1>
+      {/* Section Header*/}
+      {/* OurGalleryText */}
+      <section className="flex flex-col container mx-auto mt-20 px-[1.5rem] md:mt-[8.13rem] md:px-0 md:mx-0 md:ml-[6.25rem]">
+        {/* Top bottom */}
+        <div className="mb-[1.12rem]">
+          <button className="bg-[#FCD1CD] text-[#1A1A1A] py-[0.5rem] font-lato text-[0.75rem] font-bold px-[1.12rem] inline rounded-full">
+            OUR GALLERY
+          </button>
+        </div>
 
-                <p className='text-[0.75rem] font-lato font-normal max-w-[23.75rem] text-[#1A1A1A] md:text-[1.5rem] md:max-w-[67.5rem]'>Your blood donation impacts accident victims and those in critical conditions. We connect people and resources nationwide, driving life-changing progress in transfusion medicine.</p>
-            </div>
-        </section>
+        {/* Text */}
+        <div className="">
+          <h1 className="text-[1.5rem] font-black text-[#1A1A1A] font-lato max-w-[17.4375rem] mb-[0.75rem] md:max-w-[67.5rem] md:text-[3rem]">
+            Different Shades of <span className="text-[#B80F00]">RedTide</span>{" "}
+            Heroes
+          </h1>
+
+          <p className="text-[0.75rem] font-lato font-normal max-w-[23.75rem] text-[#1A1A1A] md:text-[1.5rem] md:max-w-[67.5rem]">
+            Your blood donation impacts accident victims and those in critical
+            conditions. We connect people and resources nationwide, driving
+            life-changing progress in transfusion medicine.
+          </p>
+        </div>
+        {/* Images  Section*/}
+
+        <div className="mt-[2.62rem]">
+          <ImageWithHover
+            imageUrl={GirlPlayground}
+            text="Heartbeat of Gratitude"
+            additionalText="A Journey of Hope and Healing through Blood Donation"
+            marginTop="20.06rem"
+          />
+          <ImageWithHover
+            imageUrl={WomanChild}
+            text="For Love's Sake"
+            additionalText="Embracing Life's Precious Moments - The Gift of Blood Donation"
+            marginTop="5rem"
+          />
+          <ImageWithHover
+            imageUrl={OldManDefault}
+            text="The Gift of Time"
+            additionalText="A Tale of Life, Love, and Second Chances with Blood Donors"
+            marginTop="5rem"
+          />
+        </div>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default OurGallery
+export default OurGallery;
---
+++ src/components/RedTideGallery/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Image from 'next/image';
+
+interface ImageWithHoverProps {
+    imageUrl: string;
+    text: string;
+    additionalText: string;
+    marginTop: string
+
+  }
+
+const ImageWithHover: React.FC<ImageWithHoverProps> = ({imageUrl, text, additionalText, marginTop}) => {
+  return (
+    <div>
+        <div className="relative">
+          <Image src={imageUrl} alt="Image" className="w-full h-auto object-cover" />
+          <div className="absolute inset-0  bg-black opacity-0 hover:opacity-70  h-auto  transition-opacity duration-500 ease-in-out">
+            <div className={`font-lato mt-[${marginTop}] ml-[1.5rem]`}>
+              <p className="text-white text-[1.12rem]">{text}</p>
+              <p className="text-white text-[0.75rem]">{additionalText}</p>
+            </div>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default ImageWithHover
