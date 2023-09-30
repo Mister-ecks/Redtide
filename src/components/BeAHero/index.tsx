26c2494dd43beb137c932a8b02be6f8a1f389d5e
Emmasaku <omosakuemmanuel22@gmail.com>
2023-09-30 16:45:53 +0100

    Adjust Hero

--- src/components/BeAHero/index.tsx
+++ src/components/BeAHero/index.tsx
@@ -10,7 +10,7 @@ const BeAHero = () => {
         <div>
           <Image src={MobileHeroBG} alt="Hero Background" className="max-w-full flex flex-shrink-0" />
           <div className={`absolute inset-0 flex justify-start items-center h-auto`}>
-            <div className={`font-lato flex flex-col justify-center ml-[1.75rem]`}>
+            <div className={`font-lato flex flex-col justify-center ml-[1.75rem] mt-[3rem]`}>
               <button className='bg-[#FCD1CD] max-w-[8.8125rem] py-[0.63rem] px-[1.5rem] text-[1.125rem] rounded-full font-bold'>BE A HERO</button>
               <h1 className='text-white text-[1.5rem] mt-[1.12rem]'>You Potentially <span className='text-[#B80F00]'>Save Three Lives <br /></span> Every Time You Donate</h1>
               <p className='text-white text-[0.75rem] mt-[0.5rem] font-normal max-w-[23.75rem]'>Embrace the power of your donation and be the hero who saves lives. in the lifesaving journey and make a lasting impact on those who depend on the generosity of donors like you. </p>
